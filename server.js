7909c72370905d7f1b4f3546c19daabc088c7427
Christian Broms <cb@christianbroms.com>
2021-04-05 11:49:11 -0400

    filter out previously visited sites

--- server.js
+++ server.js
@@ -3,13 +3,32 @@ const fetch = require("node-fetch");
 const HTMLParser = require("node-html-parser");
 const puppeteer = require("puppeteer");
 
-const scrapeLinks = async (page, location, originsToIgnore) => {
-  try {
-    const thisLinkOrigin = new URL(location).origin;
+const getTLD = (location) => {
+  const hostname = new URL(location).hostname;
+  if (hostname === null || hostname === "") return null;
+  const parts = hostname.split(".");
+  return `${parts[parts.length - 2]}.${parts[parts.length - 1]}`;
+};
 
+const scrapeLinks = async (page, location, visitedTLDs) => {
+  try {
+    const thisLinkTLD = getTLD(location);
+    if (thisLinkTLD === null) throw new Error("Bad location");
+
+    visitedTLDs.add(thisLinkTLD);
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+      deviceScaleFactor: 1,
+    });
     await page.goto(location, {
       waitUntil: "networkidle2",
     });
+    // sometimes a url is a redirect, in which case we want to get the final url
+    const settledLocation = page.url();
+    const settledTLD = getTLD(settledLocation);
+    visitedTLDs.add(settledTLD);
+
     //await page.screenshot({ path: "example.png" });
     // get all the links on the page
     const hrefs = await page.$$eval("a", (links) =>
@@ -18,12 +37,9 @@ const scrapeLinks = async (page, location, originsToIgnore) => {
     await page.close();
     // filter out any links that are from the same site or any of the previous sites we've visited
     const filteredHrefs = hrefs.filter((link) => {
-      const linkOrigin = new URL(link).origin;
+      const linkTLD = getTLD(link);
       return (
-        linkOrigin !== null &&
-        linkOrigin !== "null" &&
-        linkOrigin !== thisLinkOrigin &&
-        originsToIgnore[thisLinkOrigin] !== true
+        linkTLD !== null && linkTLD !== thisLinkTLD && !visitedTLDs.has(linkTLD)
       );
     });
     return filteredHrefs;
@@ -54,30 +70,36 @@ const scrapeLinks = async (page, location, originsToIgnore) => {
 const maxDepth = 2;
 const start = "https://www.molleindustria.org/";
 
-const recursivelyScrape = async (browser, location, originsToIgnore, depth) => {
+const recursivelyScrape = async (browser, location, visitedTLDs, depth) => {
   if (depth > maxDepth) return;
   else {
+    const thisLocationTLD = getTLD(location);
+    if (thisLocationTLD === null) return;
+
     console.log(`getting links from ${location}...`);
 
     // scape the links from the page
     const page = await browser.newPage();
-    const links = await scrapeLinks(page, location, originsToIgnore);
+    const links = await scrapeLinks(page, location, visitedTLDs);
+    console.log(`links from ${location}:`);
     console.log(links);
     console.log(`found ${links.length}`);
 
     // don't return to this website
-    const thisLinkOrigin = new URL(location).origin;
-    originsToIgnore[thisLinkOrigin] = true;
+    visitedTLDs.add(thisLocationTLD);
 
     // for each valid link, recursively scrape it
-    for (const link of links) {
-      await recursivelyScrape(browser, link, originsToIgnore, depth + 1);
-    }
+    const scrapers = links.map((link) => {
+      return recursivelyScrape(browser, link, visitedTLDs, depth + 1);
+    });
+
+    return await Promise.allSettled(scrapers);
   }
 };
 
 (async () => {
   const browser = await puppeteer.launch();
-  const results = await recursivelyScrape(browser, start, [], 0);
+  const visitedTLDs = new Set();
+  const results = await recursivelyScrape(browser, start, visitedTLDs, 0);
   await browser.close();
 })();
