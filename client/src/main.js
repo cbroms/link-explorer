a7049228caf15409dfa593bd8b4545a035bf6a61
Christian Broms <cb@christianbroms.com>
2021-05-02 15:22:15 -0400

    load backend connection string from env file

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@ import App from "./App.svelte";
 const app = new App({
   target: document.body,
   props: {
-    backend: "ws://localhost:3000",
+    backend: _app.env.API || "ws://localhost:3000",
   },
 });
 
